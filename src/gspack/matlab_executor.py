752bb29cfe7c770ba3718584d0238c40cefe4476
Aleksei Sholokhov <aksh@uw.edu>
2020-12-16 11:58:15 -0800

    Add matlab2python converter

--- src/gspack/matlab_executor.py
+++ src/gspack/matlab_executor.py
@@ -1,7 +1,29 @@
 import matlab.engine  # This import can only be the first line in the file (*fpm*)
 from matlab.engine import MatlabExecutionError  # it's NOT an error: it will be imported once matlab.engine is.
+import numpy as np
+
+import matlab
 from gspack.helpers import UserFailure, GspackFailure
 
+
+def matlab2python(a):
+    matlab_array_types = (matlab.double,
+                          matlab.single,
+                          matlab.int8,
+                          matlab.int16,
+                          matlab.int32,
+                          matlab.int64,
+                          matlab.uint8,
+                          matlab.uint16,
+                          matlab.uint32,
+                          matlab.uint64
+                          )
+    if type(a) in matlab_array_types:
+        return np.array(a, dtype=float)
+    elif type(a) == int or type(a) == float or type(a) == str:
+        return a
+    else:
+        raise ValueError(f"Unknown MATLAB type: {type(a)}")
 
 def get_from_workspace(workspace, key, default=None):
     try:
@@ -33,7 +55,7 @@ def execute_matlab(file_path, matlab_settings):
         for name in matlab_settings["variables_to_take"]:
             item = get_from_workspace(eng.workspace, name)
             if item is not None:
-                workspace[name] = item
+                workspace[name] = matlab2python(item)
         eng.quit()
         return workspace
     except Exception as e:
