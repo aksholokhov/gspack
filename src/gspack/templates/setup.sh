eaca18b954f60c8cd841a42f47c038280df1820b
Aleksei Sholokhov <aksh@uw.edu>
2020-12-23 15:40:07 -0800

    version up

--- src/gspack/templates/setup.sh
+++ src/gspack/templates/setup.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
 # Install python
-apt-get update
-apt-get install -y python python3 python3-pip python3-dev python3.7 jq git libxt6
+apt-get install -y python python3 python3-pip python3-dev jq
 # Install gspack dependencies
-python3.7 -m pip install subprocess32 numpy scipy matplotlib
+pip3 install subprocess32 numpy scipy matplotlib gspack
 # Install solution script dependencies
-python3.7 -m pip install -r /autograder/source/requirements.txt
-# Sometimes pipreqs erroneously pulls matlab into requirements.txt
-python3.7 -m pip uninstall matlab
-# Install gspack
-git clone https://github.com/aksholokhov/gspack.git
-cd gspack || exit
-python3.7 setup.py install
-cd ..
+pip3 install -r /autograder/source/requirements.txt
 
 matlab=$(jq '.matlab_support' /autograder/source/config.json)
 if [ $matlab = 1 ]; then
@@ -26,7 +18,7 @@ fi
 jupyter=$(jq '.jupyter_support' /autograder/source/config.json)
 if [ $jupyter = 1 ]; then
     echo "Adding Jupyter components"
-    python3.7 -m pip install ipython nbformat
+    pip3 install ipython nbformat
 fi
 
 echo "Main setup.sh completed"
